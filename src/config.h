3ef553398994ca9fa7dcf4c60abde0e6f695963e
Stephen Cochrane <skippy@localhost.localdomain>
2020-05-22 18:01:55 +0200

    Added rofi keybind

--- src/config.h
+++ src/config.h
@@ -58,8 +58,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *rofi[] = {"rofi","-show", "run", NULL};
 
 static Key keys[] = {
+	{ MODKEY,                       XK_i,      spawn,          {.v = rofi } },
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
