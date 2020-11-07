7a0f488ae359d23ffab3150ffe43b874d80ec2cc
Stephen Cochrane <ohmyskippy@disroot.org>
2020-11-07 22:27:22 +0200

    Can now move items in the stack

--- src/config.h
+++ src/config.h
@@ -110,6 +110,7 @@ static const char *xscreensaver[] = {"xscreensaver-command","-activate", NULL};
 static const char *clipmenu[] = {"clipmenu", NULL};
 static const char *clipdel[] = {"clipdel", "-d", ".*", NULL};
 
+#include "movestack.c"
 static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = clipmenu } },
@@ -120,8 +121,13 @@ static Key keys[] = {
 	//{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+
+	/* Stack */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+  	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
